aac816352a7ff7a18580ae4af2b1b5586c8c7317
Nate Butler <iamnbutler@gmail.com>
2021-11-20 17:58:10 -0500

    How Does Routing in Nuxt Work?

---
+++ nuxt.config.js
@@ -0,0 +1,15 @@
+export default {
+  components: true,
+  head: {
+    titleTemplate: "Mastering Nuxt: %s",
+    htmlAttrs: {
+      Lang: "en"
+    },
+    bodyAttrs: {
+      class: "test-style"
+    },
+    meta: [{
+      charset: "utf-8"
+    }]
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div v-for="home in homes" :key="home.objectID">
+      <HomeCard :home="home" />
+    </div>
+  </div>
+</template>
+
+<script>
+import homes from "~/data/homes";
+
+export default {
+  head() {
+    return {
+      title: "Homepage",
+      meta: [
+        {
+          name: "description",
+          content: "this is a homepage",
+          hid: "description",
+        },
+      ],
+    };
+  },
+  data() {
+    return {
+      homes: homes.slice(0, 3),
+    };
+  },
+};
+</script>
